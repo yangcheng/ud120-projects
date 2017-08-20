e7e7a75557595b477b79c999bd3a0fe0a5dd313b
Cheng Yang <cheng@uber.com>
2017-08-20 16:02:51 -0700

    finish Naive Bayes, with accuray 0.973

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,8 +26,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+gnb.fit(features_train,labels_train)
 
+from sklearn.metrics import accuracy_score
 
+print accuracy_score(gnb.predict(features_test),labels_test)
 #########################################################
 
 
