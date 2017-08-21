98094b9f2ddef71d3cf8c384fdd4b8b0a5199e84
Cheng Yang <cheng@uber.com>
2017-08-20 21:55:51 -0700

    decision tree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -24,8 +24,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
-
+print len(features_train[0])
+clf = tree.DecisionTreeClassifier(min_samples_split=40)
+clf.fit(features_train,labels_train)
+print accuracy_score(clf.predict(features_test),labels_test)
 #########################################################
 
 
