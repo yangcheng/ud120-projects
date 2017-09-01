f76efa70bbb11d025720980c557c3131e0f4b079
Cheng Yang <cheng@uber.com>
2017-08-31 22:01:58 -0700

    text learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -34,32 +34,41 @@ word_data = []
 ### can take a long time
 ### temp_counter helps you only look at the first 200 emails in the list so you
 ### can iterate your modifications quicker
-temp_counter = 0
+#temp_counter = -sys.maxint
 
 
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
+#        temp_counter += 1
+#        if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
-
+            words = parseOutText(email)
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            words = words.replace('sara','')
+            words =words.replace('shackleton','')
+            words =words.replace('chris','')
+            words =words.replace('germani','')
 
             ### append the text to word_data
-
+            word_data.append(words)
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
+            if name == 'sara':
+                from_data.append(0)
+            elif name == 'chris':
+                from_data.append(1)
 
             email.close()
 
 print "emails processed"
+print word_data[152]
+
 from_sara.close()
 from_chris.close()
 
@@ -69,7 +78,11 @@ pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
 
-
 ### in Part 4, do TfIdf vectorization here
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(stop_words="english")
+vectorizer.fit_transform(word_data)
+print len(vectorizer.get_feature_names())
 
 
